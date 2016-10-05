ac6838b2d1031a3a59a8e923c1a92a6da3759308
Aaron Conklin <conklin.aaron@gmail.com>
2016-10-05 17:00:35 -0400

    Me Ruining Everything

--- scripts.js
+++ scripts.js
@@ -2,27 +2,49 @@ $(function(){
 
 function run() {
 // Create variables for objects I want to manipulate
-var screen = $('.screen');
+var screen = $('body');
 var player = $('#player');
 
 // Use parseInt to get values to be more consistent
 var screen_width = parseInt(screen.width());
-var playerStart = parseInt(player.css('left'));
-var speed = 5;
+var speed = 20;
 
 // Function for my runner moving left to right in my screen
     setInterval(function() {
-    var playerNow = parseInt(player.css('left'));
-    if (playerNow >= screen_width) {
-      playerNow = playerStart.css('right', playerNow + speed);
+    var playerRight = parseInt(player.css('left'));
+    if (playerRight > (screen_width - 20)) {
+      playerRight();
       }
 // Make the player's position move from left to right at the set speed
-    player.css('left', playerNow + speed);
+    player.css('left', playerRight + speed);
+
   }, 40);
-};
+}
+
+function collide() {
+      var player = $('#player');
+      var wall = $()
+}
 
 $(document).ready(function(){
   run();
 })
 
 })
+
+
+// $(function(){
+
+//     function moveLeftRight() {
+
+//       var player = $('#player');
+
+//       var playerCurrent = player.animate({left: '1219px'}, 4000);
+
+//     }
+
+//     $(document).ready(function(){
+//     moveLeftRight();
+// })
+
+// })
