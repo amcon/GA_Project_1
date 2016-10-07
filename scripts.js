a6829f4f9e76e21f6934cbd40fe66ffa06783d9c
Aaron Conklin <conklin.aaron@gmail.com>
2016-10-07 15:04:39 -0400

    Collision Pt. 1

--- scripts.js
+++ scripts.js
@@ -2,16 +2,27 @@ $(function(){
 
 // create variables in global scope
 
-var $player = $("#player");
-var $screen = $("#screen");
-var xAxis = 5;
-var yAxis = 5;
+var $player = $(".player");
+var $screen = $("body");
+var $container1 = $(".container1");
+var $container2 = $(".container2");
+var $container3 = $(".container3");
+
+var $floor = $(".floorLong");
+var $hole = $(".hole");
+var $pillar1 = $(".pillar1");
+var $pillar2 = $(".pillar2");
+var $pillar3 = $(".pillar3");
+
+var xAxis = 2;
+var yAxis = 1;
 
 var $wall = $(".wall");
 
 var interval = null;
 var interval2 = null;
-
+var counter = 0
+var falling = true;
 
 //----------------------------------------------------------------------//
 
@@ -20,7 +31,7 @@ var interval2 = null;
           function movePlayer(){
 
           var xPosition = parseInt($player.css("left"));
-          var screenWidth = parseInt($screen.width())
+          var screenWidth = parseInt($screen.width());
 
           if (xPosition > (screenWidth - 20) || xPosition < (screenWidth - screenWidth)){
           xAxis = -xAxis;
@@ -37,11 +48,45 @@ var interval2 = null;
 
          function fall(){
 
+              var xPosition = parseInt($player.css("left"));
               var yPosition = parseInt($player.css("top"));
+
+              var container1Height = parseInt($container1.height());
+              var floorHeight = parseInt($floor.height());
+              var floorWidth = parseInt($floor.width());
+              var holeHeight = parseInt($hole.height());
+              var holeWidth = parseInt($hole.width());
+              var pillar1Height = parseInt($pillar1.height());
+              var pillar1Width = parseInt($pillar1.width());
+
+
+              // if he falls into a div floor top, quit the fall funtion, but move forward
+              // else, keep falling
+              if (xPosition >= (floorWidth - 20)) {
+                    var falling = true;
+                    if (yPosition >= 125) {
+                          // clearInterval(intFall);
+                          yAxis = 0;
+                          var falling = false;
+                          }
+                    }
+
+               // if he falls into a hole top, you lose
+               // if (xPosition >= holeWidth) {
+               //      if (yPosition >= container1Height - holeHeight);
+               //      clearInterval(intFall);
+               //      clearInterval(inMove);
+               //      alert("You LOSE!");
+               // }
+
+              //else if (yPosition < container1Height - floorHeight) - 25) && (yPosition < ((container1Height - pillar1Height) - 25)) {
+              //   setInterval(intFall);
+              // }
 
               var pxValue = yPosition + yAxis + "px";
               $player.css("top", pxValue);
-         }
+              console.log(yPosition);
+          }
 
 
 //----------------------------------------------------------------------//
@@ -87,7 +132,9 @@ var interval2 = null;
 
           function jump(){
                   $player.animate({top: 80}, 400);
+                  // $player.css("margin-top", "-40px");
                       };
+
 
 
 //----------------------------------------------------------------------//
@@ -95,6 +142,11 @@ var interval2 = null;
 //  create a function for when the player hits the winZone that alerts that he won/reloads the page.
 
           // function homeSafe(){
+
+            //if (you touch winZone){
+            //   alert("YOU WIN!")
+            // }
+
 
           // }
 
@@ -104,7 +156,7 @@ var interval2 = null;
 
 var intMove = setInterval(function() {
     movePlayer();
-    }, 40);
+    }, 15);
 
 // var intId = setInterval(function() {
 //     hitWall();
@@ -118,9 +170,9 @@ $(document).keydown(function(e){
     }
    })
 
-setInterval(function(){
+var intFall = setInterval(function(){
   fall();
-}, 40);
+}, 25);
 
 
 
